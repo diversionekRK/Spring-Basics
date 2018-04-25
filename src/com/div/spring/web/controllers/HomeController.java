63c31408781f31ac1f64f629cd37333357abaec5
RK <rafal.klewek@pollub.edu.pl>
2018-04-25 17:08:07 +0200

    Try how logging works

--- src/com/div/spring/web/controllers/HomeController.java
+++ src/com/div/spring/web/controllers/HomeController.java
@@ -1,6 +1,7 @@
 package com.div.spring.web.controllers;
 
 import com.div.spring.web.dao.Offer;
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,8 +15,13 @@ import java.util.List;
 @Controller
 public class HomeController {
 
+    Logger logger = Logger.getLogger(HomeController.class);
+
     @RequestMapping("/")
     public String showHome() {
+
+        logger.info("Using logging from home controller.");
+
         return "home";
     }
 }
